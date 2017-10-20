a3641bb04ba5d4fc9da48a42ee192f69dd0493d8
davinaScott <32618166+davinaScott@users.noreply.github.com>
2017-10-20 01:51:18 -0500

    Completed Project
    It Works :D

--- maze.js
+++ maze.js
@@ -1,18 +1,41 @@
+lost = false;
+
 window.onload = function() {
-	document.getElementById("status").innerHTML = "check";
-	document.querySelectorAll(".boundary").onmouseover = allBoundaries();
+	var mStart = document.getElementById("start");
+	mStart.addEventListener('click', start);
+	var mEnd = document.getElementById("end");
+	mEnd.addEventListener('mouseover', youWin);
+	var mLeave = document.getElementById("maze");
+	mLeave.addEventListener('mouseleave', youLose);
+	var boundaries = document.querySelectorAll("#maze div.boundary");
+    for (var x = 0; x < boundaries.length; x++) {
+        boundaries[x].onmouseover = youLose;
+    }
 }
 
-function youlose(){
-	document.getElementById("boundary1").classList.toggle("youlose");
-	var cList = document.getElementById("boundary1").classList;
-}
-
-function allBoundaries(){
-	var boundaries = document.querySelectorAll(".boundary");
+function youLose(){
+	lost = true;
+	var boundaries = document.querySelectorAll("#maze div.boundary");
 	for (var x = 0; x < boundaries.length; x++) {
-		boundaries[x].classList.toggle("youlose");
+		boundaries[x].classList.add("youlose");
+	}
+	document.getElementById("status").innerHTML = "You Lose. Better Luck Next Time..";
+}
+
+function youWin(){
+	if (!lost){
+		document.getElementById("status").innerHTML = "You Win!!";
+	}	
+}
+
+function start(){
+	lost = false;
+	document.getElementById("status").innerHTML = "Find Your Way To The End!!";
+	var boundaries = document.querySelectorAll("#maze div.boundary");
+	for (var x = 0; x < boundaries.length; x++) {
+		boundaries[x].classList.remove("youlose");
 	}
 }
+
 
 
